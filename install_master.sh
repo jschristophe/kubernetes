fdb9e6c7f9920c5240e846ecdb8b7c11cce1c6df
jschristophe <jschristophe@gmail.com>
2019-03-26 19:56:05 +0100

    Update install_master.sh

--- install_master.sh
+++ install_master.sh
@@ -33,11 +33,11 @@ sudo kubeadm init
 
 #############CONFIG####################
 
-#mkdir -p $HOME/.kube
+mkdir -p $HOME/.kube
 
-#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 
-#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 sudo cp /etc/kubernetes/admin.conf $HOME/
 
